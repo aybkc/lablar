dac1c2b0446acaf431d5935482a70e9e0962f5a4
aybkc <149118795+aybkc@users.noreply.github.com>
2023-11-01 01:43:24 +0400

    Add files via upload
    LAB 2 BİLGİSAYAR

---
+++ Ayberk Koç LAB2.c
@@ -0,0 +1,175 @@
+/*Plaka kodlarından şehir ismini gösteren program*/
+#include <stdio.h>
+
+int main()
+{
+    int plaka_kodu;
+    printf ("Plaka kodu giriniz \n");
+    scanf ("%d", &plaka_kodu);
+    
+    if (plaka_kodu==1)
+    printf ("ADANA\n");
+else if (plaka_kodu==2)
+printf ("ADIYAMAN\n");
+else if (plaka_kodu==3)
+printf ("AFYONKARAHİSAR\n");
+else if (plaka_kodu==4)
+printf ("AĞRI\n");
+else if (plaka_kodu==5)
+printf ("AMASYA\n");
+else if (plaka_kodu==6)
+printf ("ANKARA\n");
+else if (plaka_kodu==7)
+printf ("ANTALYA\n");
+else if (plaka_kodu==8)
+printf ("ARTVİN\n");
+else if (plaka_kodu==9)
+printf ("AYDIN\n");
+else if (plaka_kodu==10)
+printf ("BALIKESİR\n");
+else if (plaka_kodu==11)
+printf ("BİLECİK\n");
+else if (plaka_kodu==12)
+printf ("BİNGÖL\n");
+else if (plaka_kodu==13)
+printf ("BİTLİS\n");
+else if (plaka_kodu==14)
+printf ("BOLU\n");
+else if (plaka_kodu==15)
+printf ("BURDUR\n");
+else if (plaka_kodu==16)
+printf ("BURSA\n");
+else if (plaka_kodu==17)
+printf ("ÇANAKKALE\n");
+else if (plaka_kodu==18)
+printf ("ÇANKIRI\n");
+else if (plaka_kodu==19)
+printf ("ÇORUM\n");
+else if (plaka_kodu==20)
+printf ("DENİZLİ\n");
+else if (plaka_kodu==21)
+printf ("DİYARBAKIR\n");
+else if (plaka_kodu==22)
+printf("EDİRNE\n");
+else if (plaka_kodu==23)
+printf ("ELAZIĞ\n");
+else if (plaka_kodu==24)
+printf ("ERZİNCAN\n");
+else if (plaka_kodu==25)
+printf ("ERZURUM\n");
+else if (plaka_kodu==26)
+printf ("ESKİŞEHİR\n");
+else if (plaka_kodu==27)
+printf ("GAZİANTEP\n");
+else if (plaka_kodu==28)
+printf ("GİRESUN\n");
+else if (plaka_kodu==29)
+printf ("GÜMÜŞHANE\n");
+else if (plaka_kodu==30)
+printf ("HAKKARİ\n");
+else if (plaka_kodu==31)
+printf ("HATAY\n");
+else if (plaka_kodu==32)
+printf ("ISPARTA\n");
+else if (plaka_kodu==33)
+printf ("MERSİN\n");
+else if (plaka_kodu==34)
+printf ("İSTANBUL\n");
+else if (plaka_kodu==35)
+printf ("İZMİR\n");
+else if (plaka_kodu==36)
+printf ("KARS\n");
+else if (plaka_kodu==37)
+printf ("KASTAMONU\n");
+else if (plaka_kodu==38)
+printf ("KAYSERİ\n");
+else if (plaka_kodu==39)
+printf ("KIRKLARELİ\n");
+else if (plaka_kodu==40)
+printf ("KIRŞEHİR\n");
+else if (plaka_kodu==41)
+printf ("KOCAELİ\n");
+else if (plaka_kodu==42)
+printf ("KONYA\n");
+else if (plaka_kodu==43)
+printf ("KÜTAHYA\n");
+else if (plaka_kodu==44)
+printf ("MALATYA\n");
+else if (plaka_kodu==45)
+printf ("MANİSA\n");
+else if (plaka_kodu==46)
+printf ("KAHRAMANMARAŞ\n");
+else if (plaka_kodu==47)
+printf ("MARDİN\n");
+else if (plaka_kodu==48)
+printf ("MUĞLA\n");
+else if (plaka_kodu==49)
+printf ("MUŞ\n");
+else if (plaka_kodu==50)
+printf ("NEVŞEHİR\n");
+else if (plaka_kodu==51)
+printf ("NİĞDE\n");
+else if (plaka_kodu==52)
+printf ("ORDU\n");
+else if (plaka_kodu==53)
+printf ("RİZE\n");
+else if (plaka_kodu==54)
+printf ("SAKARYA\n");
+else if (plaka_kodu==55)
+printf ("SAMSUN\n");
+else if (plaka_kodu==56)
+printf ("SİİRT\n");
+else if (plaka_kodu==57)
+printf ("SİNOP\n");
+else if (plaka_kodu==58)
+printf ("SİVAS\n");
+else if (plaka_kodu==59)
+printf ("TEKİRDAĞ\n");
+else if (plaka_kodu==60)
+printf ("TOKAT\n");
+else if (plaka_kodu==61)
+printf ("TRABZON\n");
+else if (plaka_kodu==62)
+printf ("TUNCELİ\n");
+else if (plaka_kodu==63)
+printf ("ŞANLIURFA\n");
+else if (plaka_kodu==64)
+printf ("UŞAK\n");
+else if (plaka_kodu==65)
+printf ("VAN\n");
+else if (plaka_kodu==66)
+printf ("YOZGAT\n");
+else if (plaka_kodu==67)
+printf ("ZONGULDAK\n");
+else if (plaka_kodu==68)
+printf ("AKSARAY\n");
+else if (plaka_kodu==69)
+printf ("BAYBURT\n");
+else if (plaka_kodu==70)
+printf ("KARAMAN\n");
+else if (plaka_kodu==71)
+printf ("KIRIKKALE\n");
+else if (plaka_kodu==72)
+printf ("BATMAN\n");
+else if (plaka_kodu==73)
+printf ("ŞIRNAK\n");
+else if (plaka_kodu==74)
+printf ("BARTIN\n");
+else if (plaka_kodu==75)
+printf ("ARDAHAN\n");
+else if (plaka_kodu==76)
+printf ("IĞDIR\n");
+else if (plaka_kodu==77)
+printf ("YALOVA\n");
+else if (plaka_kodu==78)
+printf ("KARABÜK\n");
+else if (plaka_kodu==79)
+printf ("KİLİS\n");
+else if (plaka_kodu==80)
+printf ("OSMANİYE\n");
+else if (plaka_kodu==81)
+printf ("DÜZCE");
+else printf ("GEÇERSİZ PLAKA KODU\n");
+
+    return 0;
+}
